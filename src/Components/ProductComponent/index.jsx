7a70b01eb9d408a40ac8b55a21db692380590dd0
Rista Chaudhary <rista.chaudhary.dev@gmail.com>
2025-03-17 22:39:55 +0530

    Added toggle on plus Btn

--- src/Components/ProductComponent/index.jsx
+++ src/Components/ProductComponent/index.jsx
@@ -1,29 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
-import { FaPlus } from "react-icons/fa";
+import { FaPlus, FaMinus } from "react-icons/fa";
 
-const ProductComponent = (props) =>{
-    return (
+const ProductComponent = (props) => {
+  const [showBuyNowBtn, setShowBuyNowBtn] = useState(true);
 
+  const handleToggleAddToCartBtn = () => {
+    setShowBuyNowBtn(!showBuyNowBtn);
+  };
 
-            <div className="product_comp_parent_buynowBtn_container">
-                 
-                 <div className="product_comp_parent_container_img_text">
-                <img src={props.image} className="product_comp_container_img"/>
-                <div className="product_comp_text"><p>{props.title}</p></div>
-             </div>
-             
-               
-            
-             <div className="product_comp_buynowBtn_Plus">
-                <button className="product_comp_buynowBtn" >Buy Now</button>
-                <div className="product_comp_PlusBtn"><FaPlus /></div>
-             </div>
-              </div>
-    
+  return (
+    <div className="product_comp_parent_buynowBtn_container">
+      <div className="product_comp_parent_container_img_text">
+        <img src={props.image} className="product_comp_container_img" />
+        <div className="product_comp_text">
+          <p>{props.title}</p>
+        </div>
+      </div>
 
-    )
-}
+      <div className="product_comp_buynowBtn_Plus">
+      {showBuyNowBtn
+      ?
+      <button className="product_comp_buynowBtn">Buy Now</button>
+      :
+      <div className="product_comp_minusBtn_quantity">
+        <div
+          className="product_comp_minusBtn"
+        >
+          <FaMinus />
+        </div>
+        <div>Quantity</div>
+      </div>
+    }
+        <div
+          className="product_comp_PlusBtn"
+          onClick={handleToggleAddToCartBtn}
+        >
+          <FaPlus />
+        </div>
+      </div>
+    </div>
+  );
+};
 export default ProductComponent;
 
 // 🔥 What This Code Does:
